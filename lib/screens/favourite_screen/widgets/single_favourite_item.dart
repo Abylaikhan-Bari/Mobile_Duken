758f3b9257071627b609cbb16dbc0d330b296b11
Abylaikhan-Bari <81789869+Abylaikhan-Bari@users.noreply.github.com>
2023-04-20 11:49:03 +0600

    En jana

--- lib/screens/favourite_screen/widgets/single_favourite_item.dart
+++ lib/screens/favourite_screen/widgets/single_favourite_item.dart
@@ -80,7 +80,7 @@ class _SingleFavouriteItemState extends State<SingleFavouriteItem> {
                           ],
                         ),
                         Text(
-                          "\$${widget.singleProduct.price.toString()}",
+                          "\₸${widget.singleProduct.price.toString()}",
                           style: const TextStyle(
                             fontSize: 18.0,
                             fontWeight: FontWeight.bold,
